1fe8a979e9efa24db30284571e3dbf289278d16e
Jakshas <jakubolin123@gmail.com>
2023-08-28 16:27:45 +0200

    Added BitBar example script to GH example repo

--- TestNG-Parallel/DemoScript.sh
+++ TestNG-Parallel/DemoScript.sh
@@ -1,2 +1,9 @@
 export PROJECT_TOKEN=<INSERT-YOUR-PROJECT_TOKEN-HERE>
-mvn clean test
+if [ "$1" == "remote" ]
+then
+  export BITBAR_KEY=<INSERT-YOUR-BITBAR_KEY-HERE>
+  export HUB_URL=<INSERT-YOUR-HUB_URL-HERE>
+  mvn clean test -Dsuite-xml=BitBarScript.xml
+else
+  mvn clean test -Dsuite-xml=DemoScript.xml
+fi
